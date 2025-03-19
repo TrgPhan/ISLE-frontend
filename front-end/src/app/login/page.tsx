eb21832887fd25190ebe1da50e88d8428832c779
TrgPhan <23020443@vnu.edu.vn>
2025-03-19 17:53:05 +0700

    Implement login and signup pages with layout adjustments and add server file

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -3,13 +3,17 @@ import Image from 'next/image';
 const LoginPage = () => {
     return (
         <div className="relative w-[1440px] h-screen bg-[#FFFFFF]">
-            <Image className='absolute top-0 left-0'
+            <Image className='absolute top-[0px] left-0 h-screen'
                 src={'/Log In or Sign Up.png'}
                 alt="Log In or Sign Up"
                 width={682}
                 height={800}
             />
-            <div className="absolute top-[100px] left-[100px]">
+            <div className="absolute top-[175px] left-[815px] flex justify-center">
+                <span className="text-black w-[289px]"
+                    style={{ fontSize: "48px", fontWeight: 700 }}>
+                    Get Started.
+                </span>
             </div>
         </div>
     );
